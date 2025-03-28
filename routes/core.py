bff92259a3c3ed9ecb1e59c56548f36da144a1f4
navetacandra <naveta.cand@gmail.com>
2025-03-28 17:27:57 +0700

    static assets downloader

--- routes/core.py
+++ routes/core.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from routes.api import api
-from routes.view import view
+from routes.view import download_static_assets, view
 
 app = Flask(__name__, static_folder="static")
 
 app.register_blueprint(api)
 app.register_blueprint(view)
+
+download_static_assets()
--- routes/view.py
+++ routes/view.py
@@ -2,10 +2,41 @@ import mimetypes
 import os
 from flask import Blueprint, abort, redirect, send_file
 from server.config import SERVER_LOCATION
+from server.install_manager import download_file
 from server.server_manager import is_installed
 
 view = Blueprint("view", __name__)
-STATIC_PATH = os.path.join(SERVER_LOCATION, "..", "static")
+STATIC_PATH = os.path.abspath(os.path.join(SERVER_LOCATION, "..", "static"))
+
+
+def download_static_assets():
+    asset_path = os.path.join(STATIC_PATH, "assets")
+    css_path = os.path.join(asset_path, "css")
+    js_path = os.path.join(asset_path, "js")
+    os.makedirs(asset_path, exist_ok=True)
+    os.makedirs(css_path, exist_ok=True)
+    os.makedirs(js_path, exist_ok=True)
+
+    files = [
+        os.path.join(css_path, "bootstrap.css"),
+        os.path.join(css_path, "bootstrap.css.map"),
+        os.path.join(js_path, "bootstrap.js"),
+        os.path.join(js_path, "bootstrap.js.map"),
+        os.path.join(js_path, "socket.io.js"),
+        os.path.join(js_path, "socket.io.js.map")
+    ]
+    urls = [
+        "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.css",
+        "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.css.map",
+        "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.js",
+        "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.js.map",
+        "https://cdn.socket.io/4.7.2/socket.io.js",
+        "https://cdn.socket.io/4.7.2/socket.io.js.map"
+    ]
+
+    for i in range(len(files)):
+        if not os.path.exists(files[i]):
+            download_file(urls[i], files[i], log=False)
 
 
 def handle_file_cache(filepath):
